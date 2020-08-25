c7ae5858551ffc1a0b62cf0774b2f28fb3b43e73
Asuka109 <1097198892@qq.com>
2020-08-25 23:14:12 +0800

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    'no-extra-semi': 'error'
+  }
+}
