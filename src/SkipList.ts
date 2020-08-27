989bb70f0df50d31eed7a4b4adfbf667a88ea766
Asuka109 <1097198892@qq.com>
2020-08-27 15:17:59 +0800

    feat(SkipListNode): Add function: seek & toArray

--- src/SkipList.ts
+++ src/SkipList.ts
@@ -18,6 +18,18 @@ export class SkipListNode {
     this.value = value
   }
 
+  seek (distance: number): NodeRef {
+    if (distance === 0) return this
+    let index: number = 0
+    let currentNode: NodeRef = this
+    while (currentNode && index !== distance) {
+      const sign = distance > 0
+      currentNode = sign ? currentNode.next : currentNode.prev
+      index += sign ? 1 : -1
+    }
+    return currentNode
+  }
+
   findBelow (value: number): SkipListNode {
     let currentNode: SkipListNode = this
     while (currentNode.next && currentNode.next.value < value) {
@@ -26,7 +38,7 @@ export class SkipListNode {
     return currentNode.down?.findBelow(value) ?? currentNode
   }
 
-  iterableRange (range: [number, number]): RangeIterator {
+  iterableRange (range: [number, number] = [-Infinity, Infinity]): RangeIterator {
     const [min, max] = range
     if (min > max) throw new Error('illegal range.')
     let currentNode = this.findBelow(min).next
@@ -52,7 +64,7 @@ export class SkipListNode {
     }
     const higherPrevNode = currentNode.up
     const higherNextNode = higherPrevNode.next
-    const higherNode = Object.assign({}, this)
+    const higherNode = new SkipListNode(this.value)
 
     this.up = higherNode
     higherNode.up = undefined
@@ -72,5 +84,14 @@ export class SkipListNode {
       currentNode.next && (currentNode.next.prev = currentNode.prev)
       currentNode = currentNode.up
     }
+  }
+
+  toArray (): Array<SkipListNode> {
+    const { next, finish } = this.iterableRange()
+    const result: Array<SkipListNode> = []
+    while (!finish()) {
+      result.push(next() as SkipListNode)
+    }
+    return result
   }
 }
